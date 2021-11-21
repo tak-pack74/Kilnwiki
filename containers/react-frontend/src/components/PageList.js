caf4f3a0a13f5142dfcc02b07586c745df5d6737
yamada-tak07 <faljojohsuke@gmail.com>
2021-11-21 18:45:56 +0900

    必要そうなコンポーネント用の空ファイルを作成 PageListの試作品を作成

--- containers/react-frontend/src/components/PageList.js
+++ containers/react-frontend/src/components/PageList.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { List, ListItem, ListItemText } from "@material-ui/core";
 
+const createData = (title, body ) => {
+  return { title, body };
+  }
+
+const pages = [
+  createData("Test1", "#hoge"),
+  createData("Test2", "##hoge"),
+  createData("Test3", "###hoge"),
+  createData("Test4", "####hoge"),
+  createData("Test5", "#####hoge"),
+];
+
 const Header = () => {
   return (
     <div className>
-    <List component="nav">
-      <ListItem>
-        <ListItemText primary="List 1" />
-      </ListItem>
-      <ListItem button>
-        <ListItemText primary="List 2 (button)" />
-      </ListItem>
-      <ListItem disabled>
-        <ListItemText primary="List 3 (disabled)" />
-      </ListItem>
-      <ListItem selected>
-        <ListItemText primary="List 4 (selected)" />
-      </ListItem>
-      <ListItem divider>
-        <ListItemText primary="List 5 (divider)" />
-      </ListItem>
-    </List>
+      <List component="nav">
+        {pages.map((page) => (
+          <ListItem button>
+          <ListItemText primary={page.title} secondary={page.body}/>
+          </ListItem>
+        ))}
+      </List>
   </div>
   );
 };
