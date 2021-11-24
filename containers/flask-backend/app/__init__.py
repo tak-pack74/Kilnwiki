23a2decc9ce9b92255d1bfa20b9892175ce63e64
yamada-tak07 <faljojohsuke@gmail.com>
2021-11-24 23:03:26 +0900

    flask側の変更。react導入のため色々不要な記述なりライブラリなりが出たので諸々削除。

--- containers/flask-backend/app/__init__.py
+++ containers/flask-backend/app/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
-bootstrap = Bootstrap()
-moment = Moment()
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -13,8 +9,6 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     
-    bootstrap.init_app(app)
-    moment.init_app(app)
     db.init_app(app)
 
     from .main import main as main_blueprint
--- containers/flask-backend/app/models.py
+++ containers/flask-backend/app/models.py
@@ -1,11 +1,18 @@
 from sqlalchemy.orm import column_property
+from sqlalchemy.sql import func
+
 from . import db
 
 class Page(db.Model):
     __tablename__ = 'page'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20))
+    title = db.Column(db.String(32))
+    desctiption = db.Column(db.String(128))
     body = db.Column(db.Text)
+    published_at = db.Column(db.DateTime)
+    edited_at = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
     
     def __repr__(self):
         return '<Role %r>' % self.name
