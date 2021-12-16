e7c2a8c84be5cd86efce4d079b40dbd2c54273bb
yamada-tak07 <faljojohsuke@gmail.com>
2021-12-16 23:46:30 +0900

    add tag table definition

--- containers/flask-backend/app/models.py
+++ containers/flask-backend/app/models.py
@@ -6,10 +6,12 @@
 
 from datetime import datetime
 
+from sqlalchemy.orm import relationship
+
 from . import db, marshmallow
 
 # 記事のタイトル・内容・登録更新日時を扱うテーブル
-# TODO 将来の拡張性のためpublished_atとcreated_at の両方を準備しているが、20211208現在未利用。
+# TODO 将来の拡張性のためpublished_atとcreated_at の両方を準備しているが、使いみちがよくわからない。
 class Page(db.Model):
     __tablename__ = 'page'
     id = db.Column(db.Integer, primary_key=True)
@@ -19,30 +21,52 @@ class Page(db.Model):
     edited_at = db.Column(db.DateTime)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime)
+
+    tag_map = relationship("PageTagMap", back_populates="page")
     
     def __repr__(self):
         return '<Page %r>' % self.title
 
-# marshmallowスキーマを生成するためのクラス
-# 主に routes.py で ページデータのシリアライズに利用する
-
-# ページ一覧
+# marshmallowスキーマを生成するためのクラス。主にroutes.py で ページデータのシリアライズに利用する
+## 全ページのスキーマ（id, titleのみ）
 class PageListSchema(marshmallow.Schema):
     class Meta:
         fields = ("id","title")
 
 pagelist_schema = PageListSchema(many=True)
 
-# 単一ページの詳細情報
+## 単一ページのスキーマ（詳細）
 class PageSchema(marshmallow.Schema):
     class Meta:
         fields = ("id","title", "body", "published_at", "edited_at")
 
 page_schema = PageSchema()
 
-
-""" 将来的に必要な Tagモデル
+# タグ。名前(name)と説明(desription)を持ち、ページと紐づく。
 class Tag(db.Model):
     __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True)
-"""
+    name = db.Column(db.String(32))
+    description = db.Column(db.String(256))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime)
+
+    page_map = relationship("PageTagMap", back_populates="tag")
+    
+    def __repr__(self):
+        return '<Tag %r>' % self.title
+
+# ページとタグの中間テーブル。いわゆるTOXI法でページとタグの関連付けを表現する
+class PageTagMap(db.Model):
+    __tablename__  = 'page_tag_map'
+    id = db.Column(db.Integer, primary_key=True)
+    page_id = db.Column(db.Integer, db.ForeignKey('page.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime)
+
+    page = relationship("page", back_populates="page_tag_map")
+    tag = relationship("tag", back_populates="page_tag_map")
+
+    def __repr__(self):
+        return '<PageTagMap %r>' % self.title
